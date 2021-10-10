e83a4a21fb7798af9cb02cbbf64cc7bad41154e3
Daniel Salazar <dhsalazar11@gmail.com>
2021-10-09 21:45:51 -0700

    Updated README.md and HelloWorldRoute.ts

--- src/routes/HelloWorldRoute.ts
+++ src/routes/HelloWorldRoute.ts
@@ -19,7 +19,7 @@ export default class HelloWorldRoute extends BaseRoute<HelloWorldResult> {
   async content(): Promise<HelloWorldResult> {
     return {
       message: 'Looks like the server is up and running!',
-      podmates: [Arianna Cooper] // HEREEEEEE!
+      podmates: ['Arianna Cooper', 'Daniel Salazar'] // HEREEEEEE!
     };
   }
 }

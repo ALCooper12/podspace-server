5e85bc20def4e5aa596b76d89f39dab82b3a718d
Arianna Cooper <60487920+ALCooper12@users.noreply.github.com>
2021-10-19 15:52:31 -0700

    fixed linting errors! (#7)
    * Altered 3 files
    
    * fixed prettier issues

--- src/routes/HelloWorldRoute.ts
+++ src/routes/HelloWorldRoute.ts
@@ -1,30 +1,3 @@
-
-import BaseRoute from '../utils/BaseRoute';
-import { RouteMethod } from '../utils/constants';
-
-type HelloWorldResult = {
-  message: string;
-  podmates: string[];
-};
-
-// This is just a "dummy" endpoint that we can send a GET request to, to ensure
-// that our server is up and running!
-export default class HelloWorldRoute extends BaseRoute<HelloWorldResult> {
-  constructor() {
-    super({
-      method: RouteMethod.GET,
-      path: '/hello'
-    });
-  }
-
-  async content(): Promise<HelloWorldResult> {
-    return {
-      message: 'Looks like the server is up and running!',
-      podmates: [Arianna Cooper] // HEREEEEEE!
-    };
-  }
-}
-
 import BaseRoute from '../utils/BaseRoute';
 import { RouteMethod } from '../utils/constants';
 
@@ -50,4 +23,3 @@ export default class HelloWorldRoute extends BaseRoute<HelloWorldResult> {
     };
   }
 }
-
